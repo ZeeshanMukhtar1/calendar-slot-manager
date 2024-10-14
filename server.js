8d522073e5ff331a4d029febf38f9db53c3fc893
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-10-14 15:17:19 +0500

    Refactor server.js to add a testing endpoint for server status check

--- server.js
+++ server.js
@@ -5,19 +5,12 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-//  testing endpoint to check if server is running
+// Testing endpoint to check if server is running
 app.get("/", (req, res) => {
-  try {
-    res.json({
-      success: true,
-      message: "Server is working fine😉",
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
+  res.json({
+    success: true,
+    message: "Server is working fine😉",
+  });
 });
 
 app.listen(port, () => {
