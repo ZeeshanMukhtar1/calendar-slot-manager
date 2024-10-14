70f0d91a0cc5ee5d7fb3c450eff5bb21e846a8fe
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-10-14 15:00:02 +0500

    Add testing endpoint to check server status

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+//  testing endpoint to check if server is running
+app.get("/", (req, res) => {
+  try {
+    res.json({
+      success: true,
+      message: "Server is working fine😉",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   console.log("Environment variables loaded:", {
